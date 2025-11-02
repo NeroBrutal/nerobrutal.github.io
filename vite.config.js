bc5a7394c7d4272595f96f00dc3ace5945923930
Mohamed Rashidh <rashid@mvp-apps.ae>
2025-11-02 22:23:27 +0400

    feat: Update homepage URL, adjust asset paths, and add new profile image

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/rashidh/", // ✅ root path for rashidh.github.io
+  base: "/",
   build: {
     outDir: "dist", // default
     assetsDir: "assets", // keep assets inside /assets/
